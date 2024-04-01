9d073b134ebc272f8c6aee603a8259305c5b0c40
Даниил Мосолов <dkuets@gmail.com>
2024-04-01 09:59:29 +0300

    remove info

--- src/components/sections/Catalog/CatalogInfo/CatalogInfo.jsx
+++ src/components/sections/Catalog/CatalogInfo/CatalogInfo.jsx
@@ -8,12 +8,6 @@ const CatalogInfo = () => {
         <section className={cl.section}>
             <Container style={{display: 'flex', flexDirection: 'column', gap: "1rem"}}>
                 <h2 className={cl.info__title}>Удобный поиск запчастей для сельскохозяйственной и прочей техники</h2>
-                <p className={cl.info__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores, autem blanditiis
-                    deserunt
-                    dignissimos, doloribus dolorum eos error esse excepturi expedita fugit illo iure iusto libero
-                    molestiae
-                    nemo nihil qui quibusdam quis ratione reprehenderit similique suscipit tempora vel velit
-                    veritatis.</p>
                 <CatalogSearch/>
             </Container>
         </section>
